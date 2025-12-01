9d9deec51ac7ebef11284fcaff62743257dd42cb
phr106-cpu <phr106@gmail.com>
2025-12-01 16:11:50 +0900

    251201

--- project/GS25/js/main.js
+++ project/GS25/js/main.js
@@ -1,1 +1,32 @@
 AOS.init();
+
+// Banner Swiper
+var bannerSwiper = new Swiper(".bannerSwiper", {
+  slidesPerView: 1.5,
+  spaceBetween: 20,
+  centeredSlides: true,
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+    clickable: true,
+  },
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  loop: true,
+});
+// Product Swiper
+var productSwiper = new Swiper(".productSwiper", {
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 20,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
